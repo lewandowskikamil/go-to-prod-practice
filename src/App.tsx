be248f293ed07fae61ab7f3b8f4caabf09c11751
Kamil Lewandowski <60607175+lewandowskikamil@users.noreply.github.com>
2025-07-01 14:19:07 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Outlet } from 'react-router-dom';
 const App = () => {
   return (
     <div>
+      <span>Just to check</span>
       <div className="bg-white rounded-lg shadow-sm p-4 mb-4">
         <h1 className="text-xl font-bold text-center">🚀 Rick and Morty - Fan Service</h1>
       </div>
